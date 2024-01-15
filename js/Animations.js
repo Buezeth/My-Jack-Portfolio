e10c6984c6b850c749cdec5227857e34a4591a0e
Chibueze <chibuezeth@gmail.com>
2024-01-15 21:15:27 +0100

    added a functional charging screen animation

--- js/Animations.js
+++ js/Animations.js
@@ -1,63 +1,44 @@
-gsap.registerPlugin(ScrollTrigger, SplitText);
+gsap.registerPlugin(ScrollTrigger);
+
+
 
 // gsap.from('.bouncing-letters', {opacity : 0, duration : 1, y : -50, stagger: 0.2, ease: "Power2.easeInOut"});
 
-// gsap.from(".article-3", {
-//     y: 100, 
-//     duration : 1,
-//     opacity: 0,
-//     scrollTrigger : {
-//         trigger: ".article-3 .bouncing-letters span",
-//         markers: true,
-//         start: "center bottom",
-//         // end: () => `+=${document.querySelector('article-3 .p-open-tag').offsetHeight}`,
-//         // toggleClass: "rebounce"
-//     }
-// })
-
-
-/* --- Split the text, Burrowing Owl --- */
-function setupSplits() {
-  
-    var tlSplitBurrowing = gsap.timeline(), 
-        SplitBurrowing = new SplitText(".article-3", {type:"char"}), 
-        chars = SplitBurrowing.chars; //an array of all the divs that wrap each character
-    
-    
-    tlSplitBurrowing.from(chars, {
-      duration: 1,
-      opacity:0,
-      y:-10,
-      ease:"circ.in",
-      stagger: 0.02,
-    scrollTrigger: {
-        trigger: ".article-2",
-        markers:true,   
-        // scrub:1,
+gsap.from(".article-3", {
+    y: 100, 
+    duration : 0.5,
+    opacity: 0,
+    scrollTrigger : {
+        trigger: ".article-3",
+        markers: true,
+        start: "top 100%",
+        end: "top top"
+        // toggleClass: "rebounce"
+    }
+})
+gsap.from(".article-4", {
+    y: 100, 
+    duration : 0.5,
+    opacity: 0,
+    scrollTrigger : {
+        trigger: ".article-4",
+        markers: true,
         start: "top center",
-       end: "top center"
-      }
-    //,   onComplete: () => {SplitBurrowing.revert()}
-    }, "+=0");
-    
-    // window.addEventListener('resize', function() {
-      // SplitBurrowing.revert();
-    // });
+        end: "top top",
+    }
+})
+gsap.from(".article-5", {
+    y: 100, 
+    duration : 0.5,
+    opacity: 0,
+    scrollTrigger : {
+        trigger: ".article-5",
+        markers: true,
+        start: "top center",
+        end: "top top",
+    }
+})
 
-      
-    };
-    
-    /*
-    ScrollTrigger.addEventListener("scrollEnd", function() {
-        SplitBurrowing.revert();
-    });
-    */
-    
-    ScrollTrigger.addEventListener("refresh", setupSplits())
-
-
-    //ScrollTrigger.addEventListener("scrollEnd", () => SplitBurrowing.revert());
-    // setupSplits();
 
     //========================================================//
     //========================================================//
@@ -70,8 +51,8 @@ function setupSplits() {
       // gsap.from('.charging', {opacity : 1, duration : 1, x : -3000, stagger: 0.2, ease: "Power2.easeOut"});
       chargingAnimation();
       setTimeout(()=>{
-        document.querySelector('.charging').style.display = "none";
-      }, 2000)
+        window.location.reload();
+      }, 1600)
     }));
 
     
@@ -82,22 +63,24 @@ function setupSplits() {
       .to('.charging', {duration : 1, x : 2000, ease: "Power2.easeOut"});
     }
 
-    //=====Progress Bar========//
-    var i = 0;
-function move() {
-  if (i == 0) {
-    i = 1;
-    var elem = document.getElementById("myBar");
-    var width = 1;
-    var id = setInterval(frame, 20);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-        i = 0;
-      } else {
-        width++;
-        elem.style.width = width + "%";
+
+    //===================================================//
+    //=============Bounce AutoPlay=======================//
+    //===================================================//
+
+    const article2 = document.querySelector(".article-4");
+
+    window.addEventListener("scroll", (event)=>{
+      let scrolled = window.scrollY;
+
+      let article2Position = article2.offsetTop;
+
+      if(scrolled == article2Position) {
+        console.log("scrolled")
+        playMe();
       }
+    })
+
+    function playMe() {
+      console.log("Just Scrolled");
     }
-  }
-}
